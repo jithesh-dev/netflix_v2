77424f8c9603674c624b3b863d932a21d58b35ab
jithesh-dev <jithesh.kumar.j@gmail.com>
2020-10-20 10:47:16 +0100

    First Commit 🚀

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Inner} from "./styles/jumbotron"
+
+const Jumbotron = () => {
+    return (
+        <Inner>
+            <h1>Jumboooo</h1>
+        </Inner>
+    )
+}
+
+export default Jumbotron
